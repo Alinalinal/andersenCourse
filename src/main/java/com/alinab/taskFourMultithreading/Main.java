aff2ebe243f156e7bb37644d7da0e944df88bbf7
Alina Bogoslavets <alinalinal@gmail.com>
2023-02-07 17:03:25 +0400

    improved task Four implementation

--- src/main/java/com/alinab/taskFourMultithreading/Main.java
+++ src/main/java/com/alinab/taskFourMultithreading/Main.java
@@ -1,64 +1,58 @@
 package com.alinab.taskFourMultithreading;
 
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Main {
+
+    private static final int NUMBER_OF_THREADS = 3;
 
     public static void main(String[] args) {
 
         MyThread thread = new MyThread();
 
+        thread.run();
+
         Thread thread1 = new Thread(thread);
         thread1.start();
 
-        Thread thread2 = new Thread(thread);
-        thread2.start();
+        Callable<Boolean> myTask = new MyThread();
 
         ExecutorService singleExecutor = Executors.newSingleThreadExecutor();
-        singleExecutor.submit(thread);
+        singleExecutor.submit(myTask);
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         ExecutorService fixedExecutor = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 3; i++) {
-            fixedExecutor.execute(thread);
+        for (int i = 1; i <= NUMBER_OF_THREADS; i++) {
+            fixedExecutor.submit(myTask);
         }
 
         ExecutorService cachedExecutor = Executors.newCachedThreadPool();
-        for (int i = 0; i < 2; i++) {
-            cachedExecutor.execute(thread);
+        for (int i = 1; i <= NUMBER_OF_THREADS; i++) {
+            cachedExecutor.submit(myTask);
         }
 
         ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(3);
-        for(int i = 0; i < 2; i++) {
-            scheduledExecutor.schedule(thread, 1, TimeUnit.SECONDS);
+        for(int i = 1; i <= NUMBER_OF_THREADS; i++) {
+            scheduledExecutor.schedule(myTask, 1, TimeUnit.SECONDS);
         }
 
         try {
             Thread.sleep(4000);
+
             singleExecutor.shutdown();
             fixedExecutor.shutdown();
             cachedExecutor.shutdown();
             scheduledExecutor.shutdown();
 
             thread1.join();
-            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        for (Map.Entry<String, Integer> entry : thread.getMap().entrySet()) {
-            System.out.println(entry.getKey() + ", resource data: " + entry.getValue());
-        }
-        System.out.println();
 
         System.out.println(thread);
     }
--- src/main/java/com/alinab/taskFourMultithreading/MyThread.java
+++ src/main/java/com/alinab/taskFourMultithreading/MyThread.java
@@ -1,36 +1,38 @@
 package com.alinab.taskFourMultithreading;
 
 import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class MyThread implements Runnable {
+public class MyThread implements Runnable, Callable<Boolean> {
 
     static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ms");
-
-    @Getter
-    Map<String, Integer> map = new ConcurrentHashMap<>();
-
-    List<List<String>> threadsInfo = new ArrayList<>();
+    static AtomicInteger COUNTER = new AtomicInteger(0);
+    static Map<String, Integer> MAP = new ConcurrentHashMap<>();
 
     @Override
     public void run() {
+        doTask();
+    }
+
+    @Override
+    public Boolean call() {
+        doTask();
+        return true;
+    }
+
+    private void doTask() {
         String threadName = Thread.currentThread().getName();
-        List<String> threadList = new ArrayList<>();
-        threadList.add(threadName + " started its work at " + LocalDateTime.now().format(FORMATTER) + "...");
-        map.put(threadName, Data.incrementAndGetResourceValue());
-        threadList.add(threadName + " finished its work at " + LocalDateTime.now().format(FORMATTER) + "!");
-        threadsInfo.add(threadList);
+        System.out.println(threadName + " started its work at " + LocalDateTime.now().format(FORMATTER) + "...");
+        MAP.put(threadName, incrementAndGetCounter());
+        System.out.println(threadName + " finished its work at " + LocalDateTime.now().format(FORMATTER) + "!");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -38,26 +40,17 @@ public class MyThread implements Runnable {
         }
     }
 
+    private int incrementAndGetCounter() {
+        return COUNTER.incrementAndGet();
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (List<String> threadInfo : threadsInfo) {
-            for (String info : threadInfo) {
-                sb.append(info).append("\n");
-            }
-            sb.append("\n");
+
+        for (Map.Entry<String, Integer> entry : MAP.entrySet()) {
+            sb.append(entry.getKey()).append(", resource data: ").append(entry.getValue()).append("\n");
         }
         return sb.toString();
-    }
-
-    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-    @ToString
-    static class Data {
-
-        static AtomicInteger dataCounter = new AtomicInteger(0);
-
-        public static int incrementAndGetResourceValue() {
-            return dataCounter.incrementAndGet();
-        }
     }
 }
